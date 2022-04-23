b3eec73609af3d7260678575008c2f93fc3da8f4
Lazyturtle0852 <103427469+Lazyturtle0852@users.noreply.github.com>
2022-04-23 22:52:34 +0900

    Update index.py

--- api/index.py
+++ api/index.py
@@ -3,7 +3,6 @@ from urllib import parse
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         s = self.path
-        dic = dict(parse.parse_qsl(parse.urlsplit(s).query))
         self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers()
