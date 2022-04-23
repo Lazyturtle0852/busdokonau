2b0e43b63a11636d084d08fa2ffbd640d26ac40f
Lazyturtle0852 <103427469+Lazyturtle0852@users.noreply.github.com>
2022-04-23 22:50:12 +0900

    Update index.py

--- api/index.py
+++ api/index.py
@@ -7,7 +7,10 @@ class handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers()
-            message = "Sucsess"
+        if "name" in dic:
+            message = "Hello, " + dic["name"] + "!"
+        else:
+            message = "Hello, stranger!"
         self.wfile.write(message.encode())
         return
 		
