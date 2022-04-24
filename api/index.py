252d43e87f32e6898829f7fa3f8f962a444be731
Lazyturtle0852 <103427469+Lazyturtle0852@users.noreply.github.com>
2022-04-24 20:17:19 +0900

    Update index.py

--- api/index.py
+++ api/index.py
@@ -22,7 +22,7 @@ class handler(BaseHTTPRequestHandler):
         except AttributeError:
           businfo1 = '本日の運行は終了しました。'
         message= businfo1
-        self.wfile.write(message.decode())
+        self.wfile.write(message.decode('utf-8'))
         try:
           bustime2 = soup.select_one('#main > div.frameArea01 > div > div > div:nth-of-type(6) > div.col02 > div.frameBox03 > p').get_text(strip=True)
           buskind2 = soup.select_one('#main > div.frameArea01 > div > div > div:nth-of-type(6) > div.col01 > table:nth-of-type(2) > tbody').get_text(strip=True)
@@ -35,7 +35,7 @@ class handler(BaseHTTPRequestHandler):
         except AttributeError:
           businfo2 = '本日の運行は終了しました。'
         message= businfo2
-        self.wfile.write(message.decode())
+        self.wfile.write(message.decode('utf-8'))
 
         try:
           bustime3 = soup.select_one('#main > div.frameArea01 > div > div > div:nth-of-type(8) > div.col02 > div.frameBox03 > p').get_text(strip=True)
@@ -48,5 +48,5 @@ class handler(BaseHTTPRequestHandler):
         except AttributeError:
           businfo3 = '本日の運行は終了しました。'
         message= businfo3
-        self.wfile.write(message.decode())
+        self.wfile.write(message.decode('utf-8'))
 
