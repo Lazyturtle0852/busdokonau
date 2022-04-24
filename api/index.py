06d7151def4d23ff559adcdb1c4a38475c6243bc
Lazyturtle0852 <103427469+Lazyturtle0852@users.noreply.github.com>
2022-04-24 10:40:46 +0900

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,3 +1,5 @@
+from flask import Flask, request, abort
+
 from http.server import BaseHTTPRequestHandler
 from urllib import parse
 class handler(BaseHTTPRequestHandler):
@@ -8,22 +10,53 @@ class handler(BaseHTTPRequestHandler):
         self.end_headers()
         self.wfile.write(message.encode())
         return 
-from urllib import request 
-from _bs4 import BeautifulSoup 
-url = 'http://real.kanachu.jp/pc/displayapproachinfo?uid=selectstop&fNM=%8F%C3%93%EC%91%E4%89w%90%BC%8C%FB&tNM=%8Cc%89%9E%91%E5%8Aw&fNO=24096&tNO=24200&x=71&y=25'
-response = request.urlopen(url)
-soup = BeautifulSoup(response)
-response.close()
-try:
-  bustime1 = soup.select_one('#main > div.frameArea01 > div > div > div:nth-of-type(4) > div.col02 > div.frameBox03 > p').get_text(strip=True)
-  buskind1 = soup.select_one('#main > div.frameArea01 > div > div > div:nth-of-type(4) > div.col01').get_text(strip=True)
-  if "ツインライナー" in buskind1:
-    buskind1 = "ツインライナー"
-  else:
-    buskind1 = "通常バス"
-  businfo1 = "一本目:"+buskind1+"での運行です。"+bustime1.replace('予定通り発車します','')
-except AttributeError:
-  businfo1 = '本日の運行は終了しました。'
-message = businfo1
-		
+        
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+)
+import os
 
+app = Flask(__name__)
+
+#環境変数取得
+YOUR_CHANNEL_ACCESS_TOKEN = os.environ["gu/2HP9iar1Fk7exqjcUQYHBH8WdwijwoYQ8miM39bmeN9prGuIJl9/KGYRCS1PmEsPFOftefr5uoMSFvJS4NvZD4eTt3I9X9mp73hFFVXpr5PkrUr8m0oMn399Y5gXnNRoGOp5XyDJ407ezOXOFGQdB04t89/1O/w1cDnyilFU="]
+YOUR_CHANNEL_SECRET = os.environ["37d7b1cfad1145e932830f360f541c23"]
+
+line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(YOUR_CHANNEL_SECRET)
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text))
+
+
+if __name__ == "__main__":
+#    app.run()
+    port = int(os.getenv("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
