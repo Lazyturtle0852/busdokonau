bea40df3bd8c3c7be26e49dab19b4e2f634c64f2
Lazyturtle0852 <103427469+Lazyturtle0852@users.noreply.github.com>
2022-04-24 18:09:40 +0900

    Update index.py

--- api/index.py
+++ api/index.py
@@ -10,4 +10,45 @@ class handler(BaseHTTPRequestHandler):
         message = cow.Cowacter().milk('Hello from Python from a Serverless Function!')
         self.wfile.write(message.encode())
         return
-message = "Shonandai is good"
+from urllib import request 
+from bs4 import BeautifulSoup  
+url = 'http://real.kanachu.jp/pc/displayapproachinfo?uid=selectstop&fNM=%8F%C3%93%EC%91%E4%89w%90%BC%8C%FB&tNM=%8Cc%89%9E%91%E5%8Aw&fNO=24096&tNO=24200&x=71&y=25'
+response = request.urlopen(url)
+soup = BeautifulSoup(response)
+response.close()
+try:
+  bustime1 = soup.select_one('#main > div.frameArea01 > div > div > div:nth-of-type(4) > div.col02 > div.frameBox03 > p').get_text(strip=True)
+  buskind1 = soup.select_one('#main > div.frameArea01 > div > div > div:nth-of-type(4) > div.col01').get_text(strip=True)
+  if "ツインライナー" in buskind1:
+    buskind1 = "ツインライナー"
+  else:
+    buskind1 = "通常バス"
+  businfo1 = "一本目:"+buskind1+"での運行です。"+bustime1.replace('予定通り発車します','')
+except AttributeError:
+  businfo1 = '本日の運行は終了しました。'
+message= businfo1
+try:
+  bustime2 = soup.select_one('#main > div.frameArea01 > div > div > div:nth-of-type(6) > div.col02 > div.frameBox03 > p').get_text(strip=True)
+  buskind2 = soup.select_one('#main > div.frameArea01 > div > div > div:nth-of-type(6) > div.col01 > table:nth-of-type(2) > tbody').get_text(strip=True)
+
+  if "ツインライナー" in buskind2:
+    buskind2 = "ツインライナー"
+  else:
+    buskind2 = "通常バス"
+  businfo2 = "二本目:"+buskind2+"での運行です。"+bustime2.replace('予定通り発車します','')
+  
+except AttributeError:
+  businfo2 = '本日の運行は終了しました。'
+message= businfo2
+try:
+  bustime3 = soup.select_one('#main > div.frameArea01 > div > div > div:nth-of-type(8) > div.col02 > div.frameBox03 > p').get_text(strip=True)
+  buskind3 = soup.select_one('#main > div.frameArea01 > div > div > div:nth-of-type(8) > div.col01 > table:nth-of-type(2) > tbody').get_text(strip=True)
+  if "ツインライナー" in buskind3:
+    buskind3 = "ツインライナー"
+  else:
+    buskind3 = "通常バス"
+  businfo3 = "三本目:"+buskind3+"での運行です。"+bustime3.replace('予定通り発車します','')
+except AttributeError:
+  businfo3 = '本日の運行は終了しました。'
+message= businfo3
+		
