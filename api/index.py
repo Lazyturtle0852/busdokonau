78937457db912d04b6d4ccb37df614f7c3d32619
Lazyturtle0852 <103427469+Lazyturtle0852@users.noreply.github.com>
2022-04-24 18:18:35 +0900

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,13 +1,11 @@
 from http.server import BaseHTTPRequestHandler
-from cowpy import cow
-
 class handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
         self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers()
-        message = cow.Cowacter().milk('Hello from Python from a Serverless Function!')
+        message = ('Hello')
         self.wfile.write(message.encode())
         return
 from urllib import request 
