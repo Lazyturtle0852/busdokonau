578acd556582159338bb95c77cd5e736fb3446c0
Lazyturtle0852 <103427469+Lazyturtle0852@users.noreply.github.com>
2022-04-24 18:49:18 +0900

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,9 +1,6 @@
 # -*- coding: cp1252 -*-
 from http.server import BaseHTTPRequestHandler
-from cowpy import cow
-
 class handler(BaseHTTPRequestHandler):
-
     def do_GET(self):
         self.send_response(200)
         self.send_header('Content-type','text/plain')
