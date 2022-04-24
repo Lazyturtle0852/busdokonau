406db38415660848bea91aa5a8d8020303661dd3
Lazyturtle0852 <103427469+Lazyturtle0852@users.noreply.github.com>
2022-04-24 10:44:45 +0900

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,16 +1,6 @@
-from flask import Flask, request, abort
 
-from http.server import BaseHTTPRequestHandler
-from urllib import parse
-class handler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        s = self.path
-        self.send_response(200)
-        self.send_header('Content-type','text/plain')
-        self.end_headers()
-        self.wfile.write(message.encode())
-        return 
-        
+# ライブラリをインポート
+from flask import Flask, request, abort
 from linebot import (
     LineBotApi, WebhookHandler
 )
@@ -18,45 +8,41 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
+    MessageEvent, TextMessage, TextSendMessage
 )
 import os
-
+#Flaskを準備
 app = Flask(__name__)
-
-#環境変数取得
-YOUR_CHANNEL_ACCESS_TOKEN = os.environ["gu/2HP9iar1Fk7exqjcUQYHBH8WdwijwoYQ8miM39bmeN9prGuIJl9/KGYRCS1PmEsPFOftefr5uoMSFvJS4NvZD4eTt3I9X9mp73hFFVXpr5PkrUr8m0oMn399Y5gXnNRoGOp5XyDJ407ezOXOFGQdB04t89/1O/w1cDnyilFU="]
-YOUR_CHANNEL_SECRET = os.environ["37d7b1cfad1145e932830f360f541c23"]
-
-line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
-handler = WebhookHandler(YOUR_CHANNEL_SECRET)
-
+#環境変数からLINE Access Tokenを設定
+LINE_CHANNEL_ACCESS_TOKEN = os.environ["Lgu/2HP9iar1Fk7exqjcUQYHBH8WdwijwoYQ8miM39bmeN9prGuIJl9/KGYRCS1PmEsPFOftefr5uoMSFvJS4NvZD4eTt3I9X9mp73hFFVXpr5PkrUr8m0oMn399Y5gXnNRoGOp5XyDJ407ezOXOFGQdB04t89/1O/w1cDnyilFU="]
+#環境変数からLINE Channel Secretを設定
+LINE_CHANNEL_SECRET = os.environ["37d7b1cfad1145e932830f360f541c23"]
+#LineBotApiのインスタンスを生成
+line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
+#WebhookHandlerのインスタンスを生成
+webhook_handler = WebhookHandler(LINE_CHANNEL_SECRET)
 @app.route("/callback", methods=['POST'])
 def callback():
-    # get X-Line-Signature header value
+    # HTTPリクエストヘッダからX-Line-Signatureを取り出す
     signature = request.headers['X-Line-Signature']
-
-    # get request body as text
+    #テキストでpostされたデータを取得
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
-
-    # handle webhook body
+    # webhookのbodyを解析する
+　#この結果はadd関数で受け取る
+　#なお、Signatureが一致していない時はInvalidSignatureError例外が発生する
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
-
     return 'OK'
-
-
+# MessageEvent
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+　　#テキストでの返信を行う
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
-
-
+        TextSendMessage(text=event.message.text )
+    )
 if __name__ == "__main__":
-#    app.run()
-    port = int(os.getenv("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run()
